64eadb3096b101bbcd22065dafdc7f190594bff6
Felipe Alfaville De Araujo <faaraujo@stefanini.com>
2017-11-24 16:22:32 -0200

    algorithm resolution

---
+++ src/hackerrank/strings/CamelCase.java
@@ -0,0 +1,27 @@
+package hackerrank.strings;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CamelCase {
+
+	private static final String PATTERN = "[A-Z]";
+	
+	private static int camelCase(String word) {		
+		if("".equals(word.trim()) && (word.length() < 1 || word.length() > Math.pow(10, 5)))
+			return 0;		
+		int result = 1;
+		Pattern pattern = Pattern.compile(PATTERN);
+		Matcher matcher = pattern.matcher(word);
+        while (matcher.find()) result++;        
+		return result;
+	}
+	
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        System.out.println(camelCase(s));
+    }
+	
+}
