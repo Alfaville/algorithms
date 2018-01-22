7b26f835c692112b2bd6eed548743475ab4c807a
Alfaville <felipealfaville@defensoria.pi.gov.br>
2018-01-21 23:24:16 -0200

    formatting

--- src/hackerrank/warmup/Staircase.java
+++ src/hackerrank/warmup/Staircase.java
@@ -3,33 +3,33 @@ package hackerrank.warmup;
 import java.util.Scanner;
 
 public class Staircase {
-	
+
 	private static final String HASHTAG = "#", SPACE = " ";
 
-    private static void staircase(int n) {    		
-	    	for (int i = 0; i < n; i++) {
-	    		System.out.println(repeat(n, i));
-	    	}    		
-    }
-    
-    private static String repeat(int n, int position) {		
+	private static void staircase(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println(repeat(n, i));
+		}
+	}
+
+	private static String repeat(int n, int position) {
 		StringBuilder sb = new StringBuilder();
 		int increment = 1;
 		for (int j = 0; j < n; j++) {
-			if((n - increment) >= position + 1)
+			if ((n - increment) >= position + 1)
 				sb.append(SPACE);
 			else
-				sb.append(HASHTAG);		
+				sb.append(HASHTAG);
 			increment++;
 		}
 		return sb.toString();
-    }
-    
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        staircase(n);
-        in.close();
-    }
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		staircase(n);
+		in.close();
+	}
 
 }
