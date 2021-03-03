dcc96b74db0174107f446d25f9188002dcd0382a
Felipe Alfaville <lipealfaville@gmail.com>
2021-03-03 00:47:28 -0300

    hackerrank - Making Anagrams

--- src/main/java/algorithms/hackerrank/strings/MakingAnagrams.java
+++ src/main/java/algorithms/hackerrank/strings/MakingAnagrams.java
@@ -1,2 +1,35 @@
-package algorithms.hackerrank.strings;public class MakingAnagrams {
+package algorithms.hackerrank.strings;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * https://www.hackerrank.com/challenges/making-anagrams/problem
+ */
+public class MakingAnagrams {
+
+    public static long execute(String s1, String s2) {
+        Map<String, Long> groupS1 = Arrays.stream(s1.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        Map<String, Long> groupS2 = Arrays.stream(s2.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        final String concatAnagrams = s1 + s2;
+        final Map<String, Long> groupHash = Arrays.stream(concatAnagrams.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        int difference = 0;
+        for (Map.Entry<String, Long> entry : groupHash.entrySet()) {
+            Long totalS2 = groupS2.getOrDefault(entry.getKey(), 0L);
+            Long totalS1 = groupS1.getOrDefault(entry.getKey(), 0L);
+
+            difference += Math.abs(totalS2 - totalS1);
+        }
+
+        return difference;
+    }
+
 }
--- src/test/java/algorithms/hackerrank/strings/MakingAnagramsTest.java
+++ src/test/java/algorithms/hackerrank/strings/MakingAnagramsTest.java
@@ -1,2 +1,39 @@
-package algorithms.hackerrank.strings;public class MakingAnagramsTest {
+package algorithms.hackerrank.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MakingAnagramsTest {
+
+    @Test
+    public void scenario1() {
+        long actual = MakingAnagrams.execute("bacdc", "dcbac");
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void scenario2() {
+        long actual = MakingAnagrams.execute("bacdc", "dcbad");
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void scenario3() {
+        long actual = MakingAnagrams.execute("abc", "amnop");
+        assertEquals(6, actual);
+    }
+
+    @Test
+    public void scenario4() {
+        long actual = MakingAnagrams.execute("cde", "abc");
+        assertEquals(4, actual);
+    }
+
+    @Test
+    public void scenario5() {
+        long actual = MakingAnagrams.execute("absdjkvuahdakejfnfauhdsaavasdlkj", "djfladfhiawasdkjvalskufhafablsdkashlahdfa");
+        assertEquals(19, actual);
+    }
+
 }
